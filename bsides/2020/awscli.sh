04a6a4f06f2068aae5d3974b4c6d85fbd1e28455
Rick Payne <40340341+rickpayne929@users.noreply.github.com>
2020-11-07 12:58:13 -0500

    pipeline ASG connection pass1

---
+++ bsides/2020/awscli.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+
+sudo apt-get update && sudo apt-get install -y curl unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
