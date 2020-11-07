456f728be5da4d1be9cc698dfdd1e4779cbc285b
Rick Payne <40340341+rickpayne929@users.noreply.github.com>
2020-11-07 00:02:18 -0500

    bsides: docker.sh digitalocean revision

--- bsides/2020/docker.sh
+++ bsides/2020/docker.sh
@@ -6,19 +6,23 @@
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
 # Install packages to allow apt to use a repository over HTTPS:
-sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 # Add Docker’s official GPG key:
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Add Docker Stable Repo
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
    
 # Update apt's package index
-sudo apt-get update
+sudo apt update
+
+# docker-ce repo check
+apt-cache policy docker-ce
 
 # Install the latest version of Docker CE and container
-sudo apt-get install -y docker-ce-cli # docker-ce-cli containerd.io (commented bc unable to locate error)
+sudo apt install -y docker-ce # docker-ce-cli containerd.io (commented bc unable to locate error)
+
+# Test
+sudo systemctl status docker
