53363d87005806d7d4415916fa484e13a949e890
Rick Payne <40340341+rickpayne929@users.noreply.github.com>
2020-11-07 00:21:37 -0500

    bsides: u18, openvpn comments

--- bsides/2020/openvpn.sh
+++ bsides/2020/openvpn.sh
@@ -1,2 +1,4 @@
-# https://www.ovpn.com/en/guides/ubuntu-cli
-apt-get install openvpn unzip
+#!/bin/bash -x
+# Reference https://www.ovpn.com/en/guides/ubuntu-cli
+#sudo apt-get update
+#sudo apt-get install openvpn unzip
