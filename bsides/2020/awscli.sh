4b893270413401a1ef3ed43b716199d0aa5d2c05
Rick Payne <40340341+rickpayne929@users.noreply.github.com>
2020-11-11 13:30:17 -0500

    bsides2020v2: sed nginx path update v2

--- bsides/2020/awscli.sh
+++ bsides/2020/awscli.sh
@@ -3,5 +3,5 @@
 sudo apt update
 sudo apt install -y unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -q awscliv2.zip
 sudo ./aws/install
